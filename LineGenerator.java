988180ef67c24a3d2d635287e4c63c1a5a37e1d4
Brittany Barnes <blbarnes@vt.edu>
2015-12-06 15:46:56 -0500

    Implemented LineGenerator.java

--- LineGenerator.java
+++ LineGenerator.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Generates a specified number of line segments.
  * 
@@ -9,18 +11,46 @@ public class LineGenerator {
 	final int LENGTH = 25;
 	final int MAX = 1000000;
 	
-	public LineGenerator() {
-		
+	public LineGenerator() 
+	{
+		// empty
 	}
 	
 	/**
 	 * Generates and returns an array of randomly generated line segments.
-	 * @param n
-	 * @return
+	 * @param n	number of line segments
+	 * @return an array of randomly generated line segments
 	 */
-	public LineSegment[] generateLines(int n) {
-		// TODO
-		return null;
+	public LineSegment[] generateLines(int n) 
+	{
+		LineSegment[] line_segments = new LineSegment[n];
+		
+		Random rand_cord = new Random();	// for generating coordinates
+		Random rand_type = new Random();	// for generating the segment type: odd=horiz; even=vert
+		
+		int x, y, type; 
+		
+		// generate the segments 
+		for (int i = 0; i < n; i++)
+		{
+			type = rand_type.nextInt(MAX + 1) + 1;	// between 1 and MAX 
+			x 	 = rand_cord.nextInt(MAX + 1);  	// between 0 and MAX
+			y 	 = rand_cord.nextInt(MAX + 1);  	// between 0 and MAX
+			
+			if (type % 2 == 0)	// even = vertical segment
+			{
+				// randomly generate the Y-coordinate of its bottom endpoint
+				VLS v_seg = new VLS(x, x, y, y + 25);
+				line_segments[i] = v_seg;
+			}
+			else	// odd = horizontal segment
+			{
+				// randomly generate the X-coordinate of its left endpoint
+				HLS h_seg = new HLS(x, x + 25, y, y);
+				line_segments[i] = h_seg; 
+			}
+		}
+		return line_segments;
 	}
 	
 }
