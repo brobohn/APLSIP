1b210bdb9e89732d15b519f184f6239ee169931a
brobohn <brobohn@vt.edu>
2015-12-06 19:46:44 -0500

    Algorithm prints

--- Algorithm.java
+++ Algorithm.java
@@ -2,14 +2,12 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
-
-
 /**
  * Runs both the brute-force algorithm and the optimized algorithm.
  *
  */
 public class Algorithm {
-	
+
 	public RandomAccessFile output_file;
 
 	public Algorithm(RandomAccessFile output_file) {
@@ -27,16 +25,16 @@ public class Algorithm {
 		int num_lines = lines.length;
 		this.output_file.writeBytes("Running the brute force algorithm. \n");
 		int num_intersections = 0;
-		//Looping through the lines array.
-		
+		// Looping through the lines array.
+
 		for (int i = 0; i < num_lines; i++) {
 			LineSegment line = lines[i];
-			boolean horiz = line instanceof HLS; 
+			boolean horiz = line instanceof HLS;
 			// Will only be comparing with lines of perpendicular direction.
 			// Looping through again.
 			for (int j = i; j < num_lines; j++) {
 				LineSegment other_line = lines[j];
-				
+
 				if (horiz && other_line instanceof VLS) {
 					// current line is horizontal and other_line is vertical.
 					// Check for intersection.
@@ -48,8 +46,8 @@ public class Algorithm {
 					int vert_low = vert_line.lower.getY();
 					int vert_high = vert_line.upper.getY();
 					int vert_x = vert_line.upper.getX();
-					
-					// checking to see if the horizontal bar is at least in 
+
+					// checking to see if the horizontal bar is at least in
 					// between the vertical bar's endpoints, vertically.
 					if (vert_low <= horz_y && horz_y <= vert_high) {
 						// If so, we need to check if they actually intersect.
@@ -59,17 +57,16 @@ public class Algorithm {
 							String line_index = Integer.toString(i);
 							String other_line_index = Integer.toString(j);
 							// point of intersection = POI
-							String POI = "(" + Integer.toString(vert_x) + ", " +
-										Integer.toString(horz_y) + ")";
+							String POI = "(" + Integer.toString(vert_x) + ", "
+									+ Integer.toString(horz_y) + ")";
 							this.output_file.writeBytes("Intersection at "
 									+ POI + " between the segment "
-									+ line_index + " and the segment " + 
-									other_line_index + ". \n");
+									+ line_index + " and the segment "
+									+ other_line_index + ". \n");
 						}
 					}
 					// If no intersection, don't do anything.
-				}
-				else if (!horiz && other_line instanceof HLS) {
+				} else if (!horiz && other_line instanceof HLS) {
 					// In this case, the current line is vertical and the other
 					// is horizontal.
 					// Check for intersection.
@@ -81,8 +78,8 @@ public class Algorithm {
 					int vert_low = vert_line.lower.getY();
 					int vert_high = vert_line.upper.getY();
 					int vert_x = vert_line.upper.getX();
-					
-					// checking to see if the vertical bar is at least in 
+
+					// checking to see if the vertical bar is at least in
 					// between the horizontal bar's endpoints, horizontally.
 					if (vert_low <= horz_y && horz_y <= vert_high) {
 						// If so, we need to check if they actually intersect.
@@ -92,25 +89,24 @@ public class Algorithm {
 							String line_index = Integer.toString(i);
 							String other_line_index = Integer.toString(j);
 							// point of intersection = POI
-							String POI = "(" + Integer.toString(vert_x) + ", " +
-										Integer.toString(horz_y) + ")";
+							String POI = "(" + Integer.toString(vert_x) + ", "
+									+ Integer.toString(horz_y) + ")";
 							this.output_file.writeBytes("Intersection at "
 									+ POI + " between the segment "
-									+ line_index + " and the segment " + 
-									other_line_index + ". \n");
+									+ line_index + " and the segment "
+									+ other_line_index + ". \n");
 						}
 					}
 					// If no intersection, don't do anything.
-				}
-				else { 
+				} else {
 					// In this case, the line segments at index i and j of the
 					// array are of the same direction and thus will not be
 					// checked for intersection.
 				}
 			}
-			
+
 		}
-		
+
 		if (num_intersections == 0) {
 			this.output_file.writeBytes("No intersections. \n\n");
 		}
@@ -129,73 +125,77 @@ public class Algorithm {
 	 * @param sweeper
 	 *            the SweepLine
 	 */
-	public void algorithm(LineSegment[] lines, EventList E, SweepLine sweeper) throws IOException {
+	public void algorithm(LineSegment[] lines, EventList E, SweepLine sweeper)
+			throws IOException {
 		// insert lines into E
 		this.output_file.writeBytes("Running the optimized algorithm. \n");
 		int num_intersections = 0;
 
 		for (LineSegment line : lines) {
 			Event e = null;
-			
-			if (line instanceof HLS) {	//HLS
+
+			if (line instanceof HLS) { // HLS
 				HLS hls = (HLS) line;
 				e = new Event(hls.left, hls.right, hls);
 				E.addEvent(e);
-			} else {					// VLS
+			} else { // VLS
 				VLS vls = (VLS) line;
-				
-				//add upper
+
+				// add upper
 				e = new Event(vls.upper, null, vls);
 				E.addEvent(e);
-				
-				//add lower
+
+				// add lower
 				e = new Event(null, vls.lower, vls);
 				E.addEvent(e);
 			}
-			
+
 		}
-		
+
 		// sort E
 		E.sort();
-		
+
 		Event event = E.getNextEvent();
-		
+
 		// for each event in E
 		while (event != null) {
-			
+
 			if (event.type == EventType.UPPER) {
 				// Upper endpoint: insert VLS into tree
-				//output_file.writeBytes("adding UPPER\n");
+				// output_file.writeBytes("adding UPPER\n");
 				sweeper.addSegment(event.segment);
-						
+
 			} else if (event.type == EventType.HORIZ) {
-				//output_file.writeBytes("checking HORIZ\n");
+				// output_file.writeBytes("checking HORIZ\n");
 				// Horizontal line: check for intersections
 				HLS hls = (HLS) event.segment;
-				
+
 				ArrayList<VLS> vlss = sweeper.printRange(event);
-				
-				//print vlss
+
+				// print vlss
 				for (VLS vls : vlss) {
 					int x = vls.getX();
 					int y = hls.getY();
-					
+
 					String POI = String.format("(%d, %d)", x, y);
-					
-					this.output_file.writeBytes("Intersection at " + POI + ". \n");
+
+					this.output_file.writeBytes("Intersection at " + POI
+							+ " between " + vls.toString() + " and "
+							+ hls.toString() + "\n");
 					num_intersections++;
 				}
-				
+
 			} else { // LOWER
-				//output_file.writeBytes("removing LOWER\n");
+				// output_file.writeBytes("removing LOWER\n");
 				// Lower endpoint: remove VLS from tree
 				sweeper.removeSegment(event.segment);
 			}
-			
-			//this.output_file.writeBytes(String.format("tree: %d nodes\n", sweeper.tree.count()));
+
+			// this.output_file.writeBytes(String.format("tree: %d nodes\n",
+			// sweeper.tree.count()));
 			event = E.getNextEvent();
 		}
-		
+
 		if (num_intersections == 0) {
 			this.output_file.writeBytes("No intersections. \n\n");
 		}
