ecc00a42e6aa6f41a9aa001d535b177e4751eb89
brobohn <brobohn@vt.edu>
2015-12-07 14:43:02 -0500

    makefile now creates jar

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash/
 
-java Driver 10
-java Driver 100
-java Driver 500
-java Driver 1000
-java Driver 2000
-java Driver 10000
-java Driver 20000
+java -jar APLSIP.jar 10
+java -jar APLSIP.jar 100
+java -jar APLSIP.jar 500
+java -jar APLSIP.jar 1000
+java -jar APLSIP.jar 2000
+java -jar APLSIP.jar 10000
+java -jar APLSIP.jar 20000
